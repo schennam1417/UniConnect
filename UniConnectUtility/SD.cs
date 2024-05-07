c710b99d22f31ac61b8376bbd0726cdff98af2af
Surekha <schennam1417@gmail.com>
2024-05-07 13:40:04 +0100

    Initial Commit

---
+++ UniConnectUtility/SD.cs
@@ -0,0 +1,15 @@
+﻿namespace UniConnectUtility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+        public static string SessionToken = "JWTToken";
+
+    }
+}
