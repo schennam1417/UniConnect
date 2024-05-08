64b31ad9f84250e894571d547d2b9d9c1d0a6f50
Surekha <schennam1417@gmail.com>
2024-05-09 00:50:09 +0100

    commit2

---
+++ Uniconnect/Extension/EnumHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Uniconnect.Helpers
+{
+    public static class EnumHelper
+    {
+        public static List<SelectListItem> GetEnumSelectList<TEnum>() where TEnum : Enum
+        {
+            return Enum.GetValues(typeof(TEnum))
+                       .Cast<TEnum>()
+                       .Select(e => new SelectListItem
+                       {
+                           Value = e.ToString(),
+                           Text = e.ToString()
+                       })
+                       .ToList();
+        }
+    }
+}
