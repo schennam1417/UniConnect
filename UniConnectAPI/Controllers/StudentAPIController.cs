fd821e2f9204c66c7e5eef47038b29d22afc4116
Surekha <schennam1417@gmail.com>
2024-05-08 14:01:33 +0100

    Working code  APIResponses

--- UniConnectAPI/Controllers/StudentAPIController.cs
+++ UniConnectAPI/Controllers/StudentAPIController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 using UniConnectAPI.Data;
 using UniConnectAPI.Models;
 using UniConnectAPI.Models.DTO;
@@ -14,47 +15,83 @@ namespace UniConnectAPI.Controllers
     public class StudentAPIController : Controller
     {
         //private readonly ApplicationDbContext _dbContext;
+        protected APIResponse _response;
         private readonly IStudentRepository _dbStudent;
         private readonly IMapper _mapper;
-        
-        public StudentAPIController(IStudentRepository dbStudent,IMapper mapper)
+
+        public StudentAPIController(IStudentRepository dbStudent, IMapper mapper)
         {
             //this._dbContext = dbContext;
             this._dbStudent = dbStudent;
             _mapper = mapper;
+            this._response = new();
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async  Task<ActionResult<IEnumerable<StudentDTO>>> GetAllStudents()
+        public async Task<ActionResult<APIResponse>> GetAllStudents()
         {
-            IEnumerable<Student> studentlist = await _dbStudent.GetAllAsync();
-            return Ok(_mapper.Map<List<StudentDTO>>(studentlist));
-           // return Ok(_dbContext.Students.ToList());
+            try
+            {
+                IEnumerable<Student> studentlist = await _dbStudent.GetAllAsync();
+                _response.Result = _mapper.Map<List<StudentDTO>>(studentlist);
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+                // return Ok(_dbContext.Students.ToList());
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
         }
 
-        [HttpGet]
-        [Route("StudentID")]
+        [HttpGet("{StudentID}",Name="GetStudent")]
+
+       // [Route("StudentID")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<StudentDTO>> GetStudent(string StudentID)
+        public async Task<ActionResult<APIResponse>> GetStudent(string StudentID)
         {
-           // var student= _dbContext.Students.SingleOrDefault(u=>u.StudentID==StudentID);
-            //return Ok(student);
-            if (StudentID == null) { return BadRequest(); }
-            var student = await _dbStudent.GetAsync(u => u.StudentID == StudentID);
-            if (student == null) { return NotFound(); }
-            return Ok(_mapper.Map<StudentDTO>(student));
+            try
+            {
+                // var student= _dbContext.Students.SingleOrDefault(u=>u.StudentID==StudentID);
+                //return Ok(student);
+                if (StudentID == null) 
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                     return BadRequest(_response); 
+                }
+                
+                var student = await _dbStudent.GetAsync(u => u.StudentID == StudentID);
 
-
+                if (student == null) 
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                     return NotFound(_response); 
+                }
+                //return Ok(_mapper.Map<StudentDTO>(student));
+                _response.Result = _mapper.Map<StudentDTO>(student);
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
         }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<StudentDTO>> AddStudent([FromBody] AddStudentDTO addstudent)
+        public async Task<ActionResult<APIResponse>> AddStudent([FromBody] AddStudentDTO addstudent)
         {
             try
             {
@@ -69,18 +106,27 @@ namespace UniConnectAPI.Controllers
                     ModelState.AddModelError("Custom Error", "Student Already Exists");
                 }
                 if(addstudent==null)
-                { return BadRequest(); }
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response); 
+                }
 
                 Student student=_mapper.Map<Student>(addstudent);
                 
                 await _dbStudent.CreateAsync(student);
-                return Ok(CreatedAtAction(nameof(GetStudent), new { StudentID = student.StudentID }, student));
+                _response.Result = _mapper.Map<StudentDTO>(student);
+                _response.StatusCode = HttpStatusCode.Created;
+                _response.IsSuccess = true;
+                //return Ok(_response);
+                //return Ok(CreatedAtAction(nameof(GetStudent), new { StudentID = student.StudentID }, student));
+                return CreatedAtRoute("GetStudent", new { StudentID = student.StudentID },_response );
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
             }
+            return _response;
         }
 
         
@@ -89,7 +135,7 @@ namespace UniConnectAPI.Controllers
         [Route("StudentID")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> UpdateStudent(string StudentID, [FromBody] UpdateStudentDTO updateStudentDTO)
+        public async Task<ActionResult<APIResponse>> UpdateStudent(string StudentID, [FromBody] UpdateStudentDTO updateStudentDTO)
         {
             //var student=_dbContext.Students.FirstOrDefault(u=>u.StudentID == StudentID);
             //if (student == null) { return NotFound("Student Not found"); }
@@ -98,18 +144,39 @@ namespace UniConnectAPI.Controllers
             //_dbContext.SaveChanges();
             //return Ok(student);
             //var student=_dbContext.Students.FirstOrDefault(u=>u.StudentID == StudentID);
-            if (updateStudentDTO == null) { return BadRequest(); }
+            try
+            {
 
-            var stdt = await _dbStudent.GetAsync(u => u.StudentID == StudentID);
+                if (updateStudentDTO == null) 
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response); 
+                }
 
-            if (stdt == null) { return NotFound(); }
-            stdt.StudentName = updateStudentDTO.StudentName;
-            stdt.WelshLanguageProficiency=updateStudentDTO.WelshLanguageProficiency;
-            //Student student = _mapper.Map<Student>(updateStudentDTO); 
+                var stdt = await _dbStudent.GetAsync(u => u.StudentID == StudentID);
 
-            await _dbStudent.UpdateAsync(stdt);
-            
-            return NoContent();
+                if (stdt == null) 
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response); 
+                }
+                stdt.StudentName = updateStudentDTO.StudentName;
+                stdt.WelshLanguageProficiency = updateStudentDTO.WelshLanguageProficiency;
+                //Student student = _mapper.Map<Student>(updateStudentDTO); 
+
+                await _dbStudent.UpdateAsync(stdt);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+
+            //return NoContent();
 
         }
                 
@@ -118,24 +185,33 @@ namespace UniConnectAPI.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Delete(String StudentID)
+        public async Task<ActionResult<APIResponse>> Delete(String StudentID)
         {
             try
             {
                 if(StudentID==null) {  return BadRequest(); }
                 var student = await _dbStudent.GetAsync(u => u.StudentID == StudentID);
-                if (student == null) { return NotFound() ; }
+                if (student == null) 
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response) ; 
+                }
                 await _dbStudent.RemoveAsync(student);
                 //var student = _dbContext.Students.FirstOrDefault(u => u.StudentID == StudentID);
                 //if (student == null) { return NotFound("Student not found"); }
                 //_dbContext.Students.Remove(student);
                 //_dbContext.SaveChanges();
-                return NoContent();
+                //return NoContent();
+                _response.StatusCode=HttpStatusCode.NoContent;
+                _response.IsSuccess=true;
+                return Ok(_response);
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
-                throw;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
             }
+            return _response;
         }
     }
 }
